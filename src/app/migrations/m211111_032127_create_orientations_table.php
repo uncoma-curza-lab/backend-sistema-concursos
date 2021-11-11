fcd4fc97ba378f6d279cbd6fcfb36c0155e712d8
Julian Murphy <njmdistrisoft@gmail.com>
2021-11-11 00:50:29 -0300

    multiple fk/constraineds

--- src/app/migrations/m211111_032127_create_orientations_table.php
+++ src/app/migrations/m211111_032127_create_orientations_table.php
@@ -3,16 +3,16 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `{{%orientation}}`.
+ * Handles the creation of table `{{%orientations}}`.
  */
-class m211111_032127_create_orientation_table extends Migration
+class m211111_032127_create_orientations_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('{{%orientation}}', [
+        $this->createTable('{{%orientations}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string(),
             'code' => $this->string(100)->notNull()->unique(),
@@ -24,6 +24,6 @@ class m211111_032127_create_orientation_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('{{%orientation}}');
+        $this->dropTable('{{%orientations}}');
     }
 }
