63db18876ba9e42656a07148f996aa70d0fb47db
Julian Murphy <njmdistrisoft@gmail.com>
2021-11-11 00:55:31 -0300

    qualif. migration

--- src/app/migrations/m211111_032217_create_postulations_table.php
+++ src/app/migrations/m211111_032217_create_postulations_table.php
@@ -14,7 +14,29 @@ class m211111_032217_create_postulations_table extends Migration
     {
         $this->createTable('{{%postulations}}', [
             'id' => $this->primaryKey(),
+            'contest_id' => $this->integer()->notNull(),
+            'person_id' => $this->integer()->notNull(),
+            'files' => $this->text(), // jsonb? .. multiples path
+            'meet_date' => $this->dateTime(),
         ]);
+
+        $this->addForeignKey(
+            'fk-postulation-person_id',
+            'postulations',
+            'person_id',
+            'persons',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-postulation-contest_id',
+            'postulations',
+            'contest_id',
+            'contests',
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
--- src/app/migrations/m211111_032231_create_qualifications_table.php
+++ src/app/migrations/m211111_032231_create_qualifications_table.php
@@ -14,7 +14,23 @@ class m211111_032231_create_qualifications_table extends Migration
     {
         $this->createTable('{{%qualifications}}', [
             'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'code' => $this->string(100)->notNull()->unique(),
+            'custom_institution' => $this->string(),
+            'custom_grade_type' => $this->string(),
+            'description' => $this->string(),
+            'file_path' => $this->string(), // path to file attach
+            'person_id' => $this->integer()->notNull(),
         ]);
+
+        $this->addForeignKey(
+            'fk-qualification-person_id',
+            'qualifications',
+            'person_id',
+            'persons',
+            'id',
+            'CASCADE'
+        );
     }
 
     /**
