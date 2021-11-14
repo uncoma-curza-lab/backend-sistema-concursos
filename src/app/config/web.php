b2c189a6b8c3993e6fd205e00c49247e18b07f38
Julian Murphy <njmdistrisoft@gmail.com>
2021-11-13 21:12:24 -0300

    application name to web config

--- src/app/config/web.php
+++ src/app/config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => $params['application_name'],
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
