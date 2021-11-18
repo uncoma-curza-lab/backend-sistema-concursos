879bace3c92a1d033b4ebd40ec260742ebb2b624
Manuel Lopez Jove <liaimasoft@gmail.com>
2021-11-18 00:16:22 -0300

    Estructura View Concurso

--- src/app/views/site/contests.php
+++ src/app/views/site/contests.php
@@ -2,51 +2,119 @@
 
 use yii\helpers\Url;
 
+if ($data!=null):
 ?>
 <div class="contaner">
-<h2>Concurso - <?= $data->name ?></h2>
+  <h2>Concurso - <?= $data->name ?></h2>
+  <div class="container">
+    <p>
+        El Centro Universitario Regional Zona Atlántica de la Universidad Nacional 
+        del Comahue comunica que se llama a concurso de antecedentes para cubrir 
+        cargos docentes para el año académico 2021.
+    </p>
+  </div>
+  <div class="container">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Departamento</th>
+          <th scope="col">Área</th>
+          <th scope="col">Orientación</th>
+          <th scope="col">Asignatura</th>
+          <th scope="col">Cargos</th>
+          <th scope="col">Categoría</th>
+          <th scope="col">Dedicación</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>      
+          <td name="departamento">Departamento</td>
+          <td name="area"><?= $data->getArea()->one()->name  ?></td>
+          <td name="orientacion"><?= $data->getOrientation()->one()->name ?></td>
+          <td name="asignatura"><?= $data->course_id ?></td>
+          <td name="cargos"><?= $data->qty ?></td>
+          <td name="categoria"><?= $data->getCategoryType()->one()->name ?></td>
+          <td name="dedicacion"><?= $data->getWorkingDayType()->one()->name ?></td>      
+        </tr>  
+      </tbody>
+    </table>
+  </div>
+  <div class="container">
+    <dl class="row">
+      <dt class="col-sm-3">Incripciones</dt>
+      <dd class="col-sm-9">
+        <ul>
+            <?php
+                $initDate = date_create($data->init_date);
+                $enrollmentDate = date_create($data->enrollment_date_end);        
+            ?>
+            <li>Se recibirán incripciones desde el <?= date_format($initDate, "d-m-Y")?> 
+            hasta el  <?= date_format($enrollmentDate, "d-m-Y")?></li>
+        </ul>
+      </dd>
+      
+      <dt class="col-sm-3">Requisitos</dt>
+      <dd class="col-sm-9">
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+            Titulo Universitario
+          </li>
+          <li class="list-group-item">
+            <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+            Curriculum Vitae con las certificaciones correspondientes
+          </li>
+          <li class="list-group-item">
+            <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+            Copia de DNI
+          </li>
+          <li class="list-group-item">
+            <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+            Residir o estar dispuesto a residir en la localidad de Viedma o aledaños
+          </li>
+          <li class="list-group-item">
+            <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+            "Propuesta de programa de la asignatura"
+          </li>
+        </ul>
+      </dd>
+      
+      <dt class="col-sm-3">Entrevista</dt>
+      <dd class="col-sm-9">
+        <p> Fecha a Determinar</p>
+      </dd>
+      
+      <dt class="col-sm-3">Informes</dt>
+      <dd class="col-sm-9">
+        <p>
+          Para Mayor información dirijirse a:
+        </p>
+        <ul>
+          <li>
+            Secretaría Academica: <a href="mailto:secretaria.academica@curza.uncoma.edu.ar">
+            secretaria.academica@curza.uncoma.edu.ar</a>
+          </li>
+          <li>
+            Departamento Docente: <a href="mailto:departamento.docente@curza.uncoma.edu.ar">
+            departamento.docente@curza.uncoma.edu.ar</a>
+          </li>
+          <li>
+            Coordinación Exactas: <a href="mailto:coordinacion.exactas@curza.uncoma.edu.ar">
+            coordinacion.exactas@curza.uncoma.edu.ar</a>
+          </li>
+        </ul>
+      </dd>
+    </dl>
 
-<p>
-    El Centro Universitario Regional Zona Atlántica de la Universidad Nacional 
-    del Comahue comunica que se llama a concurso de antecedentes para cubrir 
-    cargos docentes para el año académico 2021.
-</p>
-
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Departamento</th>
-      <th scope="col">Área</th>
-      <th scope="col">Orientación</th>
-      <th scope="col">Asignatura</th>
-      <th scope="col">Cargos</th>
-      <th scope="col">Categoría</th>
-      <th scope="col">Dedicación</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>      
-      <td name="departamento">Departamento</td>
-      <td name="area"><?= $data->area_id  ?></td>
-      <td name="orientacion"><?= $data->orientation_id ?></td>
-      <td name="asignatura"><?= $data->course_id ?></td>
-      <td name="cargos"><?= $data->qty ?></td>
-      <td name="categoria"><?= $data->category_type_id ?></td>
-      <td name="dedicacion"><?= $data->working_day_type_id ?></td>      
-    </tr>  
-  </tbody>
-</table>
-
-<h3>Incripciones:</h3>
-<ul>
-    <?php
-        $date = date_create($data->enrollment_date_end);        
-    ?>
-    <li>Hasta el:  <?= date_format($date, "d-m-Y")?></li>
-</ul>
-
-
-<a href="<?= Url::toRoute('site/contact') ?>" 
-class="btn btn-success"><i class="bi bi-person-plus-fill"></i> Insribirse </a>
+  </div>
+  <br>
+  <a href="<?= Url::toRoute('site/contact') ?>" 
+  class="btn btn-success"><i class="bi bi-person-plus-fill"></i> Insribirse </a>
 
 </div>
+<?php else: ?>
+<div class="container">
+  <h2>Concurso NO Encontrado</h2>
+</div>
+<?php endif; ?>
+
