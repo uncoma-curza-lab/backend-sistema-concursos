064194df1a70dee29895c9b9562496ab63af609e
Julian Murphy <njmdistrisoft@gmail.com>
2021-12-11 20:00:57 -0300

    add course and career

---
+++ src/app/models/Career.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace app\models;
+
+use app\services\SPCService;
+
+class Career
+{
+    protected $name;
+    protected $code;
+
+
+    public function __construct(string $name, string $code)
+    {
+        $this->name = $name;
+        $this->code = $code;
+    }
+
+    /**
+     * Departaments from SPC
+     * @throws Exception if empty or error service
+     * @return Collection data
+     */
+    public static function all()
+    {
+        $service = new SPCService();
+        $careers = $service->getAll('carrera');
+
+        if ($careers['code'] >= 400) {
+            throw new \Exception('Model error');
+        }
+        // TODO: implement mapper
+
+
+        $collect = json_decode($careers['data']);
+        $collect = array_map(function($career){
+            $entity = new self(
+                $career->nombre,
+                $career->id
+            );
+            return $entity;
+        }, $collect);
+
+        return $collect;
+    }
+
+    public static function findByDepartament($departamentID)
+    {
+        $service = new SPCService();
+        $careers = $service->getAll('carrera/departamento?id=' . $departamentID);
+
+        if ($careers['code'] >= 400) {
+            throw new \Exception('Model error, status:'. $careers['code'] . $careers['data']);
+        }
+
+        $collect = json_decode($careers['data']);
+        $collect = array_map(function($career){
+            $entity = new self($career->nombre, $career->id);
+            return $entity;
+        }, $collect);
+        return $collect;
+    }
+
+
+    public static function find($id)
+    {
+        $service = new SPCService();
+        $departaments = $service->getOne('carrera', $id);
+
+        if ($departaments['code'] >= 400) {
+            throw new \Exception('Model error');
+        }
+
+        $departamentRawData = json_decode($departaments['data']);
+        $departament = new self(
+            $departamentRawData->nombre,
+            $departamentRawData->id
+        );
+        return $departament;
+    }
+
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
