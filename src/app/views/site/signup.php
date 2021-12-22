2aebc522a65bd2cace0dd275196caa1d2d03cb81
Julian Murphy <njmdistrisoft@gmail.com>
2021-12-22 15:22:20 -0300

    button trls

--- src/app/views/site/signup.php
+++ src/app/views/site/signup.php
@@ -28,7 +28,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                    <?= Html::submitButton(\Yii::t('app', 'signup_title'), ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
