671cc28f34098f98070ea7d424a92c973808f9ea
Julian Murphy <njmdistrisoft@gmail.com>
2021-12-22 14:33:41 -0300

    fix validator

--- src/app/models/SignupForm.php
+++ src/app/models/SignupForm.php
@@ -1,10 +1,14 @@
 <?php
 namespace app\models;
 
+use app\models\traits\ValidateCUIL;
 use yii\base\Model;
 
 class SignupForm extends Model
 {
+    
+    use ValidateCUIL;
+
     public $first_name;
     public $last_name;
     public $username;
@@ -28,14 +32,15 @@ class SignupForm extends Model
 
             ['username', 'trim'],
             ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'validateCUIL'],
+            //['username', 'unique', 'targetClass' => User::class, 'message' => 'This username has already been taken.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 
             ['email', 'trim'],
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            //['email', 'unique', 'targetClass' => User::class, 'message' => 'This email address has already been taken.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
@@ -65,12 +70,12 @@ class SignupForm extends Model
         }
         
         $user = new User();
-        $user->username = $this->username;
-        $user->email = $this->email;
-        $user->setPassword($this->password);
-        $user->generateAuthKey();
-        $user->generateEmailVerificationToken();
-        
-        return $user->save() && $this->sendEmail($user) ? $user : null;
+        //$user->username = $this->username;
+        //$user->email = $this->email;
+        //$user->setPassword($this->password);
+        //$user->generateAuthKey();
+        //$user->generateEmailVerificationToken();
+        //
+        //return $user->save() && $this->sendEmail($user) ? $user : null;
     }
 }
--- src/app/models/traits/ValidateCUIL.php
+++ src/app/models/traits/ValidateCUIL.php
@@ -9,19 +9,22 @@ namespace app\models\traits;
  */
 trait ValidateCUIL {
 
+
     public function validateCUIL($cuit, $params, $validator)
     {
+        if (!$this->isValidCUIL($this->$cuit)) {
+            $this->addError($cuit, 'Verifique el número de identificación');
+        }
+
+    }
+
+    private function isValidCUIL($cuit)
+    {
         $digits = array();
-		if (strlen($cuit) != 13) return false;
+		if (strlen($cuit) != 11) return false;
 		for ($i = 0; $i < strlen($cuit); $i++) {
-			if ($i == 2 or $i == 11) {
-				if ($cuit[$i] != '-') return false;
-			} else {
-				if (!ctype_digit($cuit[$i])) return false;
-				if ($i < 12) {
-					$digits[] = $cuit[$i];
-				}
-			}
+			if (!ctype_digit($cuit[$i])) return false;
+			$digits[] = $cuit[$i];
 		}
 		$acum = 0;
 		foreach (array(5, 4, 3, 2, 7, 6, 5, 4, 3, 2) as $i => $multiplicador) {
@@ -30,7 +33,7 @@ trait ValidateCUIL {
 		$cmp = 11 - ($acum % 11);
 		if ($cmp == 11) $cmp = 0;
 		if ($cmp == 10) $cmp = 9;
-		return ($cuit[12] == $cmp);
+		return ($cuit[10] == $cmp);
     }
 
 }
