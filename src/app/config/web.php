3aee29c5e6b635515c0562512fc9b20b559ed28e
Julian Murphy <njmdistrisoft@gmail.com>
2021-12-22 15:44:10 -0300

    auth manager

--- src/app/config/web.php
+++ src/app/config/web.php
@@ -33,6 +33,9 @@ $config = [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
