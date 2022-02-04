b0cf5a261939d37175032e546691baad661ca4b3
Julian Murphy <njmdistrisoft@gmail.com>
2022-02-04 10:29:16 -0300

    and in where

--- src/app/models/ContestsQuery.php
+++ src/app/models/ContestsQuery.php
@@ -45,7 +45,7 @@ class ContestsQuery extends \yii\db\ActiveQuery
     public function onlyPublic() : self
     {
         return $this->where(['in', 'contest_status_id', ContestStatus::publicContestStatus()])
-                    ->where(['<=', 'init_date', date('Y-m-d H:i:s')]);
+                    ->andWhere(['<=', 'init_date', date('Y-m-d H:i:s')]);
     }
 
     public function sortBy(array $columns) : self
