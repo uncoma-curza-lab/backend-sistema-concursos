3a61440512da5cc7e1393853d3c613f46fd1e7bc
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-03-14 20:34:12 -0300

    fix errores

--- src/app/controllers/PostulationsController.php
+++ src/app/controllers/PostulationsController.php
@@ -12,8 +12,6 @@ use yii\helpers\Url;
 use yii\web\Controller;
 use kartik\mpdf\Pdf;
 
-use function Safe\file_get_contents;
-
 class PostulationsController extends Controller
 {
     /**
@@ -124,32 +122,21 @@ class PostulationsController extends Controller
             'contest' => $contest
         ]);
         
-        // setup kartik\mpdf\Pdf component
+        
         $pdf = new Pdf([
-            // set to use core fonts only
             'mode' => Pdf::MODE_CORE, 
-            // A4 paper format
             'format' => Pdf::FORMAT_A4, 
-            // portrait orientation
             'orientation' => Pdf::ORIENT_PORTRAIT, 
-            // stream to browser inline
             'destination' => Pdf::DEST_BROWSER, 
-            // your html content input
             'content' => $content,  
-            // format content from your own css file if needed or use the
-            // enhanced bootstrap css built by Krajee for mPDF formatting 
             'cssFile' => '@vendor/kartik-v/yii2-mpdf/src/assets/kv-mpdf-bootstrap.min.css',
-            // any css to be embedded if required
             'cssInline' => '.kv-heading-1{font-size:18px}', 
-            // set mPDF properties on the fly
             'options' => ['title' => 'Comprobante de Postulación'],
-            // call mPDF methods on the fly
             'methods' => [ 
                 'SetHeader'=>['Universidad Nacional Del Comahue'],
             ]
         ]);
         
-        // return the pdf output as per the destination setting
         return $pdf->render(); 
         
     }
