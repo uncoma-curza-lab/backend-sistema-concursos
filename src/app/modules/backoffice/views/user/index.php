f4a463228d6295c766ea8c26b3a519d70ba95cfc
Julian Murphy <njmdistrisoft@gmail.com>
2022-04-11 00:36:50 -0300

    numeric not

--- src/app/modules/backoffice/views/user/index.php
+++ src/app/modules/backoffice/views/user/index.php
@@ -25,7 +25,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
             'id',
             'uid',
