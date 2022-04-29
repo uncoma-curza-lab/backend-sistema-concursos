7707b0a7c51f6a8439cd99b54b0d36c9456537bc
Julian Murphy <njmdistrisoft@gmail.com>
2022-04-28 21:54:17 -0300

    delete use

--- src/app/config/spc.php
+++ src/app/config/spc.php
@@ -1,5 +1,5 @@
 <?php
-use yii\helpers\Url;
+
 defined('SPC_API_URL') or define('SPC_API_URL', $_ENV['SPC_API_URL'] ?? 'http://spc_api_failed');
 defined('BASE_URL') or define('BASE_URL', $_ENV['BASE_URL'] ?? 'http://127.0.0.1:8000');
 
