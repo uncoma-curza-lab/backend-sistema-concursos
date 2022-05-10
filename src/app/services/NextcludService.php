d5bc693810e23f24cbb14e5227fdcaf990fd8a41
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-05-10 20:04:15 -0300

    creacion de archivo NextcloudService.php

---
+++ src/app/services/NextcludService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\services;
+
+class NextcludService
+{
+    use GuzzleTrait;
+
+    protected $url;
+    protected $headers;
+
+    public function __construct()
+    {
+        $this->url = \Yii::$app->params['spc']['url'];
+        $this->headers = [
+            //'Authorization' => 'Basic ' . $this->token,
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+    }
+
+    public function createFolder(string $folder)
+    {
+
+    }
+/*
+    public function getAll(string $endpoint) : array
+    {
+        try {
+            $response = self::exec(
+                $this->url . '/' . $endpoint,
+                null,
+                $this->headers,
+                'GET'
+            );
+
+            return [
+                'code' => $response->getStatusCode(),
+                'data' => $response->getBody()->getContents(),
+            ];
+        } catch (\Exception $e) {
+            return [
+                'code' => 500,
+                'data' => 'Error en el servidor',
+            ];
+
+        }
+    }
+
+    public function getOne(string $endpoint, string $id) : array
+    {
+        try {
+            $response = self::exec(
+                $this->url . '/' . $endpoint . '/' . $id,
+                null,
+                $this->headers,
+                'GET'
+            );
+
+            return [
+                'code' => $response->getStatusCode(),
+                'data' => $response->getBody()->getContents(),
+            ];
+        } catch (\Exception $e) {
+            return [
+                'code' => 500,
+                'data' => 'Error en el servidor',
+            ];
+
+        }
+
+    }
+}*/
