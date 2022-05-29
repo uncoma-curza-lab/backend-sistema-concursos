f04bbfaa3d528a39a4a0e864dc0006e95f484b24
Julian Murphy <njmdistrisoft@gmail.com>
2022-05-28 23:30:01 -0300

    add migration and comments

---
+++ src/app/migrations/m220529_002337_add_activity_to_contest.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220529_002337_add_activity_to_contest
+ */
+class m220529_002337_add_activity_to_contest extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $createType = <<<SQL
+            CREATE TYPE activities AS ENUM ('TEACHER', 'DEPARTMENT_ASSISTANT');
+        SQL;
+        $createField = <<<SQL
+            ALTER TABLE contests ADD activity activities;
+        SQL;
+        $this->execute($createType);
+        $this->execute($createField);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $deleteField = <<<SQL
+            ALTER TABLE contests DROP activity;
+        SQL;
+        $deleteType = <<<SQL
+            DROP TYPE activities;
+        SQL;
+        $this->execute($deleteField);
+        $this->execute($deleteType);
+    }
+
+}
