2b2f1adedd7c7cb980d8c1b1a6d911495214b652
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-08-04 13:09:17 -0300

    aply boostrap class to upload-resolution view

--- src/app/modules/backoffice/views/contest/upload_resolution.php
+++ src/app/modules/backoffice/views/contest/upload_resolution.php
@@ -1,19 +1,16 @@
 <?php
-
-use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use kartik\select2\Select2;
-
 ?>
 
 <div class="contests-form">
 
-<h3>Cambiar estado del concurso: <i> <?= $model->name ?></i></h3>
+<h3>Cargar Dictamen del concurso: <i> <?= $model->name ?></i></h3>
 <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]) ?>
 
-    <?= $form->field($modelForm, 'resolution_file_path')->fileInput() ?>
-
-    <button>Enviar</button>
+<div class="mb-3">
+    <?= $form->field($modelForm, 'resolution_file_path')->fileInput(['class'=>"form-control"])->label('Dictamen:', ['class'=>'form-label']) ?>
+</div>
+    <button class="btn btn-success">Enviar</button>
 
 <?php ActiveForm::end() ?>
 
