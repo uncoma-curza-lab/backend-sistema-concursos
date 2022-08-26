4fb8ed0784ef9dfce388fe34dce4ad427dbce683
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-08-26 15:13:32 -0300

    add a statues compare method

--- src/app/models/PostulationStatus.php
+++ src/app/models/PostulationStatus.php
@@ -10,9 +10,24 @@ class PostulationStatus
     const PENDING = 'pending';
     const ACCEPTED = 'accepted';
     const REJECTED = 'rejected';
+    const STATUSES = [self::DRAFT, self::PENDING, self::ACCEPTED, self::REJECTED];
 
     public static function getTranslation(string $status) : string
     {
         return Yii::t('models/postulation-status', $status);
+    }
+
+    public static function isEqualStatus($compareStatus, $selfStatus) : bool
+    {
+        if(!self::isValidStauts($selfStatus)){
+            throw new \Exception("Error: The param $selfStatus is not a valid Postulation Status");
+        }
+
+        return $compareStatus == $selfStatus;
+    }
+
+    private static function isValidStauts($status) : bool
+    {
+        return in_array($status, self::STATUSES);
     }
 }
