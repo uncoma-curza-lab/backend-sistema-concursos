913e82d5f342eabbeef4844cdec55ff15683a7ac
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-09-01 14:37:14 -0300

    clean contest details view

--- src/app/modules/backoffice/views/contest/contest_details.php
+++ src/app/modules/backoffice/views/contest/contest_details.php
@@ -8,9 +8,6 @@ use yii\bootstrap4\Html;
 use yii\helpers\Url;
 use yii\web\View;
 
-$this->params['breadcrumbs'][] = ['label' => Yii::t('backoffice', 'contests'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = $view;
-
 $contestLinks = [];
 $loggedUser = Yii::$app->user;
 $roles = null;
@@ -18,32 +15,28 @@ if ($loggedUser) {
     $roles = array_keys(Yii::$app->authManager->getRolesByUser($loggedUser->id));
 }
  if (in_array('admin', $roles) || in_array('jury', $roles) || in_array('teach_departament', $roles)) {
-     //Notify when have postulations on pending status
-     $notify = $params['model']->hasPendingPostulations() ? 'style="color: #E58B16;"' : '';
      array_push($contestLinks, Html::a(
          '<i class="bi bi-eye-fill" aria-hidden="true"></i><span class="vnav-title" style="display: none"> Detalles</span>',
          Url::to(['/backoffice/contest/view', 'slug' => $params['model']->code]),
          ['title' => 'Detalles', 'class' => 'nav-link vnav-link px-0']
      ));
- }
- if (in_array('admin', $roles) || in_array('jury', $roles) || in_array('teach_departament', $roles)) {
-     //Notify when have postulations on pending status
+
+      //Notify when have postulations on pending status
      $notify = $params['model']->hasPendingPostulations() ? 'style="color: #E58B16;"' : '';
      array_push($contestLinks, Html::a(
          '<i class="bi bi-person-lines-fill" ' . $notify . ' aria-hidden="true"></i><span class="vnav-title" style="display: none"> Postulaciones</span>',
          Url::to(['postulation/contest', 'slug' => $params['model']->code]),
          ['title' => 'Postulaciones', 'class' => 'nav-link vnav-link px-0']
      ));
- }
- if (in_array('admin', $roles) || in_array('jury', $roles) || in_array('teach_departament', $roles)) {
-     array_push($contestLinks, Html::a(
+
+      array_push($contestLinks, Html::a(
          '<i class="bi bi-folder-fill" aria-hidden="true"></i><span class="vnav-title" style="display: none"> Archivos</span>',
          ['contest/contest-files', 'contestId' => $params['model']->id],
          ['title' => 'Archivos', 'class' => 'nav-link vnav-link px-0']
      ));
  }
 ?>
-<div class="contests-create">
+<div class="contests-details">
        <div class="row align-items-center">
          <div id="column" class="col-md-auto">
              <nav class="nav flex-column vnav">
@@ -55,24 +48,23 @@ if ($loggedUser) {
                ?>
              </nav>
          </div>
-         <div class="col vnav-side">
+         <div class="col">
              <?= $this->render($view, $params) ?>
          </div>
        </div>
 </div>
 <?php 
 $sideNavBar = <<< 'JS'
-    
 let show = false;
 $('.vnav').hover(() => {
     if(!show){
-        $('.vnav-title').show(1000, () => {
+        $('.vnav-title').show(200, () => {
             show = true
         })
        }
     }, () => {
     if(show){
-        $('.vnav-title').hide(1000, () => {
+        $('.vnav-title').hide(200, () => {
             show = false
         })
         }
