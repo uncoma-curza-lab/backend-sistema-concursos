80d68ca9e76fc82fce7792e4bdb0eac7137d66cd
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-09-01 10:10:25 -0300

    make de nav links

--- src/app/modules/backoffice/views/contest/contest_details.php
+++ src/app/modules/backoffice/views/contest/contest_details.php
@@ -4,18 +4,47 @@
 /* @var $this yii\web\View */
 /* @var $model app\models\Contests */
 
+use yii\bootstrap4\Html;
+use yii\helpers\Url;
+
 $this->params['breadcrumbs'][] = ['label' => Yii::t('backoffice', 'contests'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $view;
+
+$contestLinks = [];
+$loggedUser = Yii::$app->user;
+$roles = null;
+if ($loggedUser) {
+    $roles = array_keys(Yii::$app->authManager->getRolesByUser($loggedUser->id));
+}
+
+ if (in_array('admin', $roles) || in_array('jury', $roles) || in_array('teach_departament', $roles)) {
+     //Notify when have postulations on pending status
+     $notify = $params['model']->hasPendingPostulations() ? 'style="color: #E58B16;"' : '';
+     array_push($contestLinks, Html::a(
+         '<span class="bi bi-person-lines-fill" ' . $notify . ' aria-hidden="true"> Postulaciones</span>',
+         Url::to(['postulation/contest', 'slug' => $params['model']->code]),
+         ['title' => 'Postulaciones', 'class' => 'nav-link']
+     ));
+     
+ }
+ if (in_array('admin', $roles) || in_array('jury', $roles) || in_array('teach_departament', $roles)) {
+     array_push($contestLinks, Html::a(
+         '<span class="bi bi-folder-fill" aria-hidden="true"> Archivos</span>',
+         ['contest/contest-files', 'contestId' => $params['model']->id],
+         ['title' => 'Archivos', 'class' => 'nav-link']
+     ));
+ }
 ?>
 <div class="contests-create">
      <div class="container">
        <div class="row">
          <div class="col-1">
              <nav class="nav flex-column">
-               <a class="nav-link active" href="#">Active</a>
-               <a class="nav-link" href="#">Link</a>
-               <a class="nav-link" href="#">Link</a>
-               <a class="nav-link disabled">Disabled</a>
+               <?php 
+                   foreach($contestLinks as $link){
+                       echo $link;
+                   }
+               ?>
              </nav>
          </div>
          <div class="col-11">
