6e2eee7f83c318aaa6a1cb78a1901e34218ecf89
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-09-01 09:06:39 -0300

    create contest details for render other views

---
+++ src/app/modules/backoffice/views/contest/contest_details.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Contests */
+
+$this->params['breadcrumbs'][] = ['label' => Yii::t('backoffice', 'contests'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $view;
+?>
+<div class="contests-create">
+     <div class="container">
+       <div class="row">
+         <div class="col-1">
+             <nav class="nav flex-column">
+               <a class="nav-link active" href="#">Active</a>
+               <a class="nav-link" href="#">Link</a>
+               <a class="nav-link" href="#">Link</a>
+               <a class="nav-link disabled">Disabled</a>
+             </nav>
+         </div>
+         <div class="col-11">
+             <?= $this->render($view, [
+                 'model' => $model,
+                 'params' => $params,
+                 ]) ?>
+         </div>
+       </div>
+     </div>
+</div>
