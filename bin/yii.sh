82aaabb0c7eab454be895814ac60317ec2662c59
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-09-22 12:17:38 -0300

    update yii script docker compose sintax

--- bin/yii.sh
+++ bin/yii.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker-compose exec app yii $@
+docker compose exec app yii $@
 
 if [ $1 == migrate/create ] 
 then
   echo "Cambiando Propietarios de ./migrations ..."
-  docker-compose exec app /bin/sh -c "chown \$PHP_USER_ID:\$PHP_USER_ID ./migrations -R" &&\
+  docker compose exec app /bin/sh -c "chown \$PHP_USER_ID:\$PHP_USER_ID ./migrations -R" &&\
    echo "Propietario modificado"
 fi
