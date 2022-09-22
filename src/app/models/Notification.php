49d15fdb60ee362e9bebfeccc061cb3df40a2962
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-09-22 12:29:22 -0300

    create notification model

---
+++ src/app/models/Notification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "notifications".
+ *
+ * @property int $id
+ * @property int $user_to
+ * @property string|null $message
+ * @property bool|null $read
+ * @property string|null $timestamp
+ *
+ * @property User $userTo
+ */
+class Notification extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'notifications';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_to'], 'required'],
+            [['user_to'], 'default', 'value' => null],
+            [['user_to'], 'integer'],
+            [['message'], 'string'],
+            [['read'], 'boolean'],
+            [['timestamp'], 'safe'],
+            [['user_to'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_to' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_to' => 'User To',
+            'message' => 'Message',
+            'read' => 'Read',
+            'timestamp' => 'Timestamp',
+        ];
+    }
+
+    /**
+     * Gets query for [[UserTo]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserTo()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_to']);
+    }
+}
