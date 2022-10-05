f2d11f3d693cca8d4a73209579121b7ee822423c
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-10-05 14:49:01 -0300

    new notification event

---
+++ src/app/events/NewPostulationEvent.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace app\events;
+
+use app\models\Contests as Contest;
+use app\models\Notification;
+use app\models\User;
+use yii\base\Event;
+
+class NewPostulationEvent extends Event implements EventInterface
+{
+    protected Contest $contest;
+    protected string $message;
+
+    public function __construct(Contest $contest){
+        $this->message = "El concurso $contest->name tiene nuevas postulaciones.";
+        $this->contest = $contest;
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $users = User::find()->getAllByRol('teach_departament');
+        foreach ($users as $user) {
+            Notification::create($user, $this->message);
+        }
+    }
+}
