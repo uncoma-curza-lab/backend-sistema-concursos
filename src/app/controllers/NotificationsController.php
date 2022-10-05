c44e393cc28ed557bc7552ef0fe6a5d2f9bdf3cb
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-10-05 15:08:30 -0300

    notifications controller

---
+++ src/app/controllers/NotificationsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\controllers;
+
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+class NotificationsController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['index'],
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('notifications', []);
+    }
+}
