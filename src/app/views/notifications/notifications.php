0c474409bbb0abc9fca21d944c203dfdb0da6cfd
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-10-05 15:08:15 -0300

    Notifications view

---
+++ src/app/views/notifications/notifications.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = Yii::t('app', 'notifications');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="notifications-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+
+</div>
+
