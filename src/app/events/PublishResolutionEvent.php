2d21069e839e99e440b6c9c7e72d8d989aa101c6
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-10-05 13:39:29 -0300

    publish resolution event

--- src/app/events/PublishResolutionEvent.php
+++ src/app/events/PublishResolutionEvent.php
@@ -12,40 +12,15 @@ class PublishResolutionEvent extends Event implements EventInterface
     protected string $message;
 
     public function __construct(Contest $contest){
-        $this->message = "El concurso .... fue publicado .....";
+        $this->message = "El concurso $contest->name a finalizado y publicado el dictamen.";
         $this->contest = $contest;
         parent::__construct();
     }
 
     public function handle()
     {
-        foreach ($this->contest->postulations as $user) {
-            $this->saveNotification($user->id);
+        foreach ($this->contest->postulations as $postulation) {
+            Notification::create($postulation->person->user, $this->message);
         }
-    }
-
-    /**
-     * @deprecated
-     */
-    public function sendNotification(string $messege)
-    {
-        $this->setMessege($messege);
-        //$this->saveNotification();
-    }
-    
-    public function setMessege(string $messege)
-    {
-        $this->message = $messege;
-    }
-
-    protected function saveNotification($user)
-    {
-        $notification = new Notification();
-        $notification->user_to = $user;
-        $notification->message = $this->message;
-        $notification->timestamp = date('Y-m-d h:m:s');
-
-        return $notification->save();
-
     }
 }
