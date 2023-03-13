bd410b669f3d6b41313398c800e95778738871fc
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-03-13 20:14:24 -0300

    view and delete files

--- src/app/modules/backoffice/views/contest/_view.php
+++ src/app/modules/backoffice/views/contest/_view.php
@@ -117,12 +117,24 @@ $this->params['breadcrumbs'][] = $this->title;
               <div class="list-group">
                   <?php 
                   foreach ($model->attachedFiles as $file): 
+                      //TODO - Delete method
                   ?>
-                      <a class="list-group-item list-group-item-action" href="<?= url::to(['@web/' . $file->path]) ?>" target="_blank">
-                          <i class="bi bi-file-earmark-text-fill" aria-hidden="true"></i>
-                          <?= $file->documentType->name ?> - 
-                          <?= $file->name ?>
-                      </a>
+                        <div class="list-group-item">
+                            <div class="row">
+                                <div class="col">
+                                  <div class="d-flex w-100 justify-content-start">
+                                    <i class="bi bi-file-earmark-text-fill" aria-hidden="true"></i>
+                                    <?= $file->documentType->name ?> - 
+                                    <?= $file->name ?>
+            
+                                  </div>
+                               </div>
+                               <div class="col-md-auto">
+                                  <a class="btn btn-success" href="<?= url::to(['@web/' . $file->path]) ?>" target="_blank" title="Ver"><i class="bi bi-eye"></i></a>
+                                  <a class="btn btn-danger" href="<?= url::to(['@web/' . $file->path]) ?>" title="Borrar"><i class="bi bi-trash"></i></a>
+                               </div>
+                            </div>
+                        </div>
                   <?php 
                   endforeach;
                   ?>
