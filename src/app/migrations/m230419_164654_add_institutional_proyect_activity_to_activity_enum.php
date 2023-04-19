7b91479627982063ca7a39ca954eebec61f667c3
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-04-19 15:12:25 -0300

    activity attr migration

---
+++ src/app/migrations/m230419_164654_add_institutional_proyect_activity_to_activity_enum.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230419_164654_add_institutional_proyect_activity_to_activity_enum
+ */
+class m230419_164654_add_institutional_proyect_activity_to_activity_enum extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        echo "Add INSTITUTIONAL_PROYECT to Activities";
+        $addActivity = <<<SQL
+            ALTER TYPE activities ADD VALUE 'INSTITUTIONAL_PROYECT';
+        SQL;
+        $this->execute($addActivity);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "Drop INSTITUTIONAL_PROYECT to Activities";
+        $deleteAtribute = <<<SQL
+            ALTER TYPE activities DROP ATTRIBUTE INSTITUTIONAL_PROYECT;
+        SQL;
+        $this->execute($deleteAtribute);
+
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230419_164654_add_institutional_proyect_activity_to_activity_enum cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
