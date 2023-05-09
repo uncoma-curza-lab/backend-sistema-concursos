caddfd2afabdb834979c125ebcecb36548edb98b
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-05-09 13:40:11 -0300

    debug and env vars on envfile

--- src/app/web/index.php
+++ src/app/web/index.php
@@ -2,8 +2,8 @@
 
 // comment out the following two lines when deployed to production
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', boolval($_ENV['YII_DEBUG']));
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
