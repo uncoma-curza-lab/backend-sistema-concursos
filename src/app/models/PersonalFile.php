b0829601e7cf1dcb94b8345616be46ec7dd6f750
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-09-06 15:48:28 -0300

    models

---
+++ src/app/models/PersonalFile.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "personal_files".
+ *
+ * @property int $id
+ * @property int|null $person_id
+ * @property int|null $postulation_id
+ * @property string $document_type_code
+ * @property string $path
+ * @property int|null $is_valid
+ * @property string|null $valid_until
+ * @property string|null $created_at
+ * @property string|null $validated_at
+ *
+ * @property DocumentsType $documentTypeCode
+ * @property Person $person
+ * @property Postulation $postulation
+ */
+class PersonalFile extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'personal_files';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['person_id', 'postulation_id', 'is_valid'], 'default', 'value' => null],
+            [['person_id', 'postulation_id', 'is_valid'], 'integer'],
+            [['document_type_code', 'path'], 'required'],
+            [['valid_until', 'created_at', 'validated_at'], 'safe'],
+            [['document_type_code', 'path'], 'string', 'max' => 255],
+            [['document_type_code'], 'exist', 'skipOnError' => true, 'targetClass' => DocumentType::class, 'targetAttribute' => ['document_type_code' => 'code']],
+            [['person_id'], 'exist', 'skipOnError' => true, 'targetClass' => Persons::class, 'targetAttribute' => ['person_id' => 'id']],
+            [['postulation_id'], 'exist', 'skipOnError' => true, 'targetClass' => Postulations::class, 'targetAttribute' => ['postulation_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'person_id' => 'Person ID',
+            'postulation_id' => 'Postulation ID',
+            'document_type_code' => 'Document Type Code',
+            'path' => 'Path',
+            'is_valid' => 'Is Valid',
+            'valid_until' => 'Valid Until',
+            'created_at' => 'Created At',
+            'validated_at' => 'Validated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[DocumentTypeCode]].
+     *
+     * @return \yii\db\ActiveQuery|DocumentsTypeQuery
+     */
+    public function getDocumentTypeCode()
+    {
+        return $this->hasOne(DocumentType::class, ['code' => 'document_type_code']);
+    }
+
+    /**
+     * Gets query for [[Person]].
+     *
+     * @return \yii\db\ActiveQuery|PersonQuery
+     */
+    public function getPerson()
+    {
+        return $this->hasOne(Persons::class, ['id' => 'person_id']);
+    }
+
+    /**
+     * Gets query for [[Postulation]].
+     *
+     * @return \yii\db\ActiveQuery|PostulationQuery
+     */
+    public function getPostulation()
+    {
+        return $this->hasOne(Postulations::class, ['id' => 'postulation_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return PersonalFileQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new PersonalFileQuery(get_called_class());
+    }
+}
---
+++ src/app/models/PersonalFileQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the ActiveQuery class for [[PersonalFile]].
+ *
+ * @see PersonalFile
+ */
+class PersonalFileQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * {@inheritdoc}
+     * @return PersonalFile[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return PersonalFile|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
