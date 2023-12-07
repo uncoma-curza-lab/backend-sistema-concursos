c39b54aa88b824170cb577e1411177b912ef8b05
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-12-07 15:13:36 -0300

    btn

--- src/app/messages/es/models/personal-files.php
+++ src/app/messages/es/models/personal-files.php
@@ -6,11 +6,14 @@ return [
     'description' => 'Descripción',
     'document_type_code' => 'Tipo de documento',
     'upload_new_file' => 'Cargar Nuevo Archivo',
+    'upload_new_personal_file' => 'Cargar Nuevo Archivo Personal',
+    'upload_new_postulation_file' => 'Cargar Nuevo Archivo de Postulación',
     'valid_indefinitely' => 'Valido',
     'valid_with_until_date' => 'Valido con fecha de vencimiento',
     'invalid' => 'Invalido',
     'validation' => 'Validez',
     'expire_date' => 'Fecha de Vencimiento',
     'unique_document_type_message' => 'Ya hay un archivo con este tipo. Solo puede haber uno.',
+    'view_my_files' => 'Ver Archivos Personales',
 ];
 
